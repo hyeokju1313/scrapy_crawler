4e8c4ac57f1f6e2e7c3964b25ddea0f96bbba433
HyeokjuLee <hyeokju1313@gmail.com>
2021-08-09 11:14:27 +0900

    delete JsonPipeline

--- 66girls/Scrapy/Scrapy_crawler/pipelines.py
+++ 66girls/Scrapy/Scrapy_crawler/pipelines.py
@@ -16,17 +16,3 @@ class CsvPipeline(object):
     def process_item(self, item, spider):
         self.exporter.export_item(item)
         return item
-
-class JsonPipeline(object):
-    def __init__(self):
-        self.file = open("url.json", 'wb')
-        self.exporter = JsonItemExporter(self.file, encoding='utf-8', ensure_ascii=False)
-        self.exporter.start_exporting()
- 
-    def close_spider(self, spider):
-        self.exporter.finish_exporting()
-        self.file.close()
- 
-    def process_item(self, item, spider):
-        self.exporter.export_item(item)
-        return item
